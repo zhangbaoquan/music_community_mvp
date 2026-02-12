e8c53aaf0ca61d42aff3a6f3f9fefba1a147f79b
coffer <zbq19930108@126.com>
2026-02-12 15:47:36 +0800

    [fix] 1、强制使用 HTML 渲染模式（兼容性好、包体积小、加载快）。2、解决手机端“首页没有内容”的问题

--- lib/features/home/home_view.dart
+++ lib/features/home/home_view.dart
@@ -28,7 +28,7 @@ class HomeView extends GetResponsiveView {
     // Mobile Layout
     return const Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
+        child: Padding(
           padding: EdgeInsets.all(20),
           child: ContentTabView(),
         ),
