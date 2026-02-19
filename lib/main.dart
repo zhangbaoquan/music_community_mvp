43e1aa44c157ed456ef116af2e35b3e6a90ae0c7
coffer <zbq19930108@126.com>
2026-02-19 17:51:40 +0800

    [fix]1、解决https 访问报错的问题

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ void main() async {
   try {
     // Initialize Supabase synchronously (well, awaited)
     await Supabase.initialize(
-      url: 'http://qinqinmusic.com', // Use Port 80 (Nginx Proxy)
+      url: 'https://qinqinmusic.com', // Use HTTPS (Cloudflare Flexible)
       anonKey:
           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE',
     );
